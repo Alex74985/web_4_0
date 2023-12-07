890fec6f20c2cd268eaf92cfaccb95923ca53927
Alexey <alekcei.zdota@mail.ru>
2023-12-07 22:20:36 +1000

    ?

--- vite-project/src/components/header/Header.jsx
+++ vite-project/src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import styles from './header.module.css';
 import Nav from "../navigation/nav/Nav.jsx";
-import {Input, Switch} from "antd";
+import {Button, Input, Switch, Modal} from "antd";
 import {useEffect, useState} from "react";
 const {Search} = Input;
+import Sign_form from "./sign_form.jsx";
 
 const Header = () => {
     const [checked, setCheck] = useState(false);
@@ -12,6 +13,15 @@ const Header = () => {
         else document.body.setAttribute('data-theme', '')
     }, [checked]);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.inner__header}>
@@ -19,6 +29,12 @@ const Header = () => {
                 <div className={styles.features}>
                     <Search className={styles.search} placeholder="input search text" enterButton/>
                     <Switch className={`${checked && styles.switch}`} onChange={() => setCheck(!checked)}/>
+                    <Button type="primary" onClick={showModal}>
+                        Sign In
+                    </Button>
+                    <Modal title="Sign In" open={isModalOpen} onCancel={handleCancel}>
+                        <Sign_form></Sign_form>
+                    </Modal>
                 </div>
             </div>
         </header>
